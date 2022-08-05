283a5d941cd72b5ec00cd6daaf93a0d1030f523f
Rafael Rodrigues de Souza <rafaelrodriguessouza10@gmail.com>
2022-08-05 12:38:40 -0300

    Update DoisUm.java

--- LearnJava/DoisouUm/src/DoisUm.java
+++ LearnJava/DoisouUm/src/DoisUm.java
@@ -4,62 +4,48 @@ public class DoisUm {
 
 	public static void main(String[] args) {
 		//Criando variáveis para o nome dos tres jogadores
-		String j1 = " ", j2 = " ", j3 = " ";
+		String jogador[] = new String [3];
 		//Criando variáveis para o valor dos três jogadores
-		int n1 = 0, n2 = 0, n3 = 0;
+		int n[] = new int[3];
 		//Criando variáveis para controle
 		int i, j;
 		//criando um objeto scanner
 		Scanner t = new Scanner(System.in);
 		
 		//Coletando o nome dos jogadores
-		for(i = 1; i < 4; i++) {
-			System.out.print("Informe o nome do " + i + "º jogador: ");
-			if(i == 1) {
-				j1 = t.nextLine();
-			} else if (i == 2) {
-				j2 = t.nextLine();
-			} else {
-				j3 = t.nextLine();
-			}
+		for(i = 0; i < 3; i++) {
+			System.out.print("Informe o nome do " + (i + 1) + "º jogador: ");
+			jogador[i] = t.nextLine();
 		}
 		
 		//Coletando o valor de cada jogador
-		i = 1;
+		i = 0;
 		do {
-			System.out.print("Informe o valor do " + i + "º jogador(2 ou 1): ");
+			System.out.print("Jogador " + jogador[i] + ", informe um valor (2 ou 1): ");
 			j = t.nextInt();
 			if(j < 1) {
 				System.out.println("Valor inválido! Insira 1 ou 2");
 			} else if (j > 2) {
 				System.out.println("Valor inválido! Insira 1 ou 2");
 			} else {
-				if(i == 1) {
-					n1 = j;
-					i++;
-				} else if (i == 2) {
-					n2 = j;
-					i++;
-				} else {
-					n3 = j;
-					i++;
-				}
+				n[i] = j;
+				i++;
 			}
 			
-		} while (i < 4);
+		} while (i < 3);
 		
 		//Comparando os valores dos jogadores e mostrando quem ganhou
-		if (n1 == n2) {
-			if(n1 == n3) {
+		if (n[0] == n[1]) {
+			if(n[0] == n[2]) {
 				System.out.print("Jogo empatado!!");
 			} else {
-				System.out.print("O vencedor é: " + j3);
+				System.out.print("O vencedor é: Jogador " + jogador[2]);
 			}
 		} else {
-			if(n1 == n3) {
-				System.out.print("O vencedor é: " + j2);
+			if(n[0] == n[2]) {
+				System.out.print("O vencedor é: Jogador " + jogador[1]);
 			} else {
-				System.out.print("O vencedor é: " + j1);
+				System.out.print("O vencedor é: Jogador " + jogador[0]);
 			}
 		}
 		t.close();
